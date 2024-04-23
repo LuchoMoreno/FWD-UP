62ea229e5e4d558d4e5fd1c4a3c5bf05626e5f3b
luchomoreno <lucho.moreno@live.com>
2024-04-23 03:04:51 -0300

    Se agregan nuevos endpoints

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,6 @@ app.post("/users",async (req,res) =>{
   let isActive = req.body.isActive;
   let password = req.body.password;
 
-  console.log(name);
-  console.log(lastname);
-
-  console.log(password);
   try{
     const result = await UsrController.addUser(name,lastname,email,isActive,password);
     if(result){
@@ -54,6 +50,64 @@ app.post("/users",async (req,res) =>{
   }  
   
 });
+
+// Modifico un usuario
+app.put("/users/:id",async (req,res) =>{
+
+  const user = { _id: req.params.id, ...req.body };
+  //             {_id: req.params.id, name: req.body.name, lastname, email }
+
+  console.log(user);
+  try{
+    
+    const result = await UsrController.editUser(user);
+    if(result){
+      res.status(200).json(result);
+    }else{
+      res.status(404).send("El usuario no existe.");
+    }  
+  }catch(error){  
+     res.status(500).send("Error");
+  } 
+
+});
+
+
+// Elimino un usuario
+app.delete("/users/:id", async(req,res) =>{
+
+  try{
+
+    const result = await UsrController.deleteUser(req.params.id);
+    if(result){
+      res.status(200).send("Usuario borrado.")
+    }else{
+      res.status(404).send("No se ha podido eliminar el usuario.")
+    }  
+
+  }catch(error){
+    res.status(500).send("Error")
+  }
+});
+
+
+// Get de todos los usuarios
+app.get("/users", async (req,res) =>{
+
+  let limit = req.query.limit;
+  let offset = req.query.offset;
+
+  try{
+      const results = await UsrController.getAllUsers(limit,offset);
+      res.status(200).json(results);
+
+  }catch(error){
+      res.status(500).send("Error. Intente más tarde.")
+  }
+
+});
+
+
 
 
 app.listen(PORT, ()=> {
